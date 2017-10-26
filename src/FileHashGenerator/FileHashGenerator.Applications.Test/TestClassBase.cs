55dfbeee6f8fcfcc5a90534c729a2ca40efbc837
jbe2277 <jbe2277@users.noreply.github.com>
2017-10-26 19:13:28 +0200

    Unit tests: replace DispatcherSynchronizationContext with UnitTestSynchronizationContext

--- src/FileHashGenerator/FileHashGenerator.Applications.Test/TestClassBase.cs
+++ src/FileHashGenerator/FileHashGenerator.Applications.Test/TestClassBase.cs
@@ -1,9 +1,8 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
-using System.Threading;
+using System.Waf.UnitTesting;
 using System.Waf.UnitTesting.Mocks;
-using System.Windows.Threading;
 using Waf.FileHashGenerator.Applications.ViewModels;
 
 namespace Test.FileHashGenerator.Applications
@@ -11,27 +10,29 @@ namespace Test.FileHashGenerator.Applications
     [TestClass]
     public abstract class TestClassBase
     {
-        private CompositionContainer container;
+        private AggregateCatalog catalog;
 
 
-        protected CompositionContainer Container => container;
+        protected UnitTestSynchronizationContext Context { get; private set; }
+
+        protected CompositionContainer Container { get; private set; }
 
 
         [TestInitialize]
         public void TestInitialize()
         {
-            SynchronizationContext.SetSynchronizationContext(new DispatcherSynchronizationContext());
+            Context = UnitTestSynchronizationContext.Create();
 
-            AggregateCatalog catalog = new AggregateCatalog();
+            catalog = new AggregateCatalog();
             catalog.Catalogs.Add(new AssemblyCatalog(typeof(ShellViewModel).Assembly));
             catalog.Catalogs.Add(new AssemblyCatalog(typeof(MockMessageService).Assembly));
             catalog.Catalogs.Add(new AssemblyCatalog(typeof(TestClassBase).Assembly));
 
-            container = new CompositionContainer(catalog, CompositionOptions.DisableSilentRejection);
+            Container = new CompositionContainer(catalog, CompositionOptions.DisableSilentRejection);
             CompositionBatch batch = new CompositionBatch();
-            batch.AddExportedValue(container);
-            container.Compose(batch);
-            
+            batch.AddExportedValue(Container);
+            Container.Compose(batch);
+
             OnTestInitialize();
         }
 
@@ -40,7 +41,9 @@ namespace Test.FileHashGenerator.Applications
         {
             OnTestCleanup();
 
-            container.Dispose();
+            Container.Dispose();
+            catalog.Dispose();
+            Context.Dispose();
         }
 
         protected virtual void OnTestInitialize() { }
